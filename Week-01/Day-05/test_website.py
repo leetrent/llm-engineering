cb6c2ca9ed78d6751dadf60d6dbd0cb465179f90
LeeTrent <lee.trent.1@gmail.com>
2025-05-28 10:43:46 -0500

    Working on Week 1 / Day 5 assignment

---
+++ Week-01/Day-05/test_website.py
@@ -0,0 +1,17 @@
+import sys
+from website import Website
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python test_site.py <URL>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    try:
+        site = Website(url)
+        print(site.links)
+    except RuntimeError as e:
+        print(f"❌ Error: {e}")
+
+if __name__ == "__main__":
+    main()
