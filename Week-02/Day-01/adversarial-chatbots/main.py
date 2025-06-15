ce5a3bacfbc35fbba35fa233e589410305b7532d
LeeTrent <lee.trent.1@gmail.com>
2025-06-15 09:10:43 -0500

    Making improvements to the interaction between ChapGPT and Claude

--- Week-02/Day-01/adversarial-chatbots/main.py
+++ Week-02/Day-01/adversarial-chatbots/main.py
@@ -2,16 +2,43 @@ from chatgpt import ChatGPT
 from claude import Claude
 
 def main():
+    
+    ################################################################################
+    # ChatGPT
+    ################################################################################
     chatGPT = ChatGPT()
     chatGPT.append_assistant_message("Hi there")
     chatGPT.append_user_message("Hi")
-    print("ChatGPT: ", chatGPT.generate_text_response())
     
+    print("\nChatGPT Initial Messages:")
+    for message in chatGPT.messages:
+        print(f"\nRole: {message['role']}\nContent: {message['content']}")
+    
+    ################################################################################        
+    # Claude
+    ################################################################################
     claude = Claude()
     claude.append_user_message("Hi there")
-    chatGPT.append_assistant_message("Hi")
-    print("Claude: ", claude.generate_text_response())
+    claude.append_assistant_message("Hi")
     
+    print("\nClaude Initial Messages:")
+    for message in claude.messages:
+        print(f"\nRole: {message['role']}\nContent: {message['content']}")
+        
+    ################################################################################
+    # ChatGPT Intial Response
+    ################################################################################
+    print("\nChatGPT (first reply): ", chatGPT.generate_text_response())
+    
+    ################################################################################
+    # Claude Intial Response
+    ################################################################################
+    print("\nClaude (first reply): ", claude.generate_text_response())
+       
+    ################################################################################
+    # Conversation Loop
+    ################################################################################
+    print("\n=== Conversation Loop ===")   
     for ii in range(5):
         gpt_next_message = chatGPT.generate_text_response()
         print(f"\n[{ii}] ChatGPT: ", gpt_next_message)
