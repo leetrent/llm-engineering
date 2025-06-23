0ad70d2277d979038c60ef50e7013d0796e79f14
LeeTrent <lee.trent.1@gmail.com>
2025-06-23 10:40:27 -0500

    Added language functionality

--- Week-02/Day-02/assignment/brochure_creation_prompts.py
+++ Week-02/Day-02/assignment/brochure_creation_prompts.py
@@ -1,17 +1,17 @@
 class BrochureCreationPrompts:
-    def __init__(self, company_name, website_content):
+    def __init__(self, company_name, website_content, language):
         self._set_system_prompt()
-        self._set_user_prompt(company_name, website_content)
+        self._set_user_prompt(company_name, website_content, language)
 
     def _set_system_prompt(self):
         self.system_prompt = (
             "You are an assistant that analyzes the contents of a company's website landing page " \
-            "and creates a brochure about the company for prospective customers, investors and recruits. "
+            "and creates a brochure about the company for prospective customers, investors and recruits in the language specified. "
             " Respond in markdown."
         )
 
-    def _set_user_prompt(self, company_name, website_content):
-        self.user_prompt = f"Please generate a company brocure for {company_name}.\n"
+    def _set_user_prompt(self, company_name, website_content, language):
+        self.user_prompt = f"Please generate a company brocure for {company_name} in {language}\n"
         self.user_prompt += "Here are the contents of company's landing page:\n"
         self.user_prompt += website_content
         self.user_prompt = self.user_prompt[:5000]  # Truncate if more than 5,000 characters
