8b566d781409e7bb7c18a40eca52df6bf9f1793d
leetrent <10787590+leetrent@users.noreply.github.com>
2025-07-03 13:40:28 -0500

    Week02 day04 airline assistant (#6)
    * Working on AI assistant. Nothing working as of yet
    
    * Got the AI airline assistant working with ChatGPT
    
    * Added more cities
    
    * Got the object oriented version of airline assistant working with Chat GPT
    
    * Got AI assistant working with Claude
    
    * Added print version chatgpt.py
    
    * Removed print statements from main_chatgpt.py
    
    * Added print version of main_claude.py
    
    * Adding print statements
    
    * Removed print statement

---
+++ Week-02/Day-04/practice-oop/ticket_prices.py
@@ -0,0 +1,15 @@
+class TicketPrices:
+    def __init__(self):
+        self.ticket_prices = {"london": "$199", 
+                              "paris": "$299", 
+                              "new york": "399", 
+                              "amsterdam": "$499",
+                              "munich": "$599",
+                              "milan": "$699",
+                              "tokyo": "$799", 
+                              "copenhagen": "$899"}
+        
+    def get_ticket_price(self, destination_city):
+        #print(f"Tool get_ticket_price called for {destination_city}")
+        city = destination_city.lower()
+        return self.ticket_prices.get(city, "Unknown")
