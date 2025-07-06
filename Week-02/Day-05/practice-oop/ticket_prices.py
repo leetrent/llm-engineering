3caa3c05bf0281d7c6638a3b0fc5c63c00ca038c
LeeTrent <lee.trent.1@gmail.com>
2025-07-06 11:25:09 -0500

    Got an object-oriented version of multi-modal Airline Ticket AI working

--- Week-02/Day-05/practice-oop/ticket_prices.py
+++ Week-02/Day-05/practice-oop/ticket_prices.py
@@ -1,10 +1,16 @@
+import json
+from openai import OpenAI
+from api_key import retrieve_api_key_value
+from image import generate_image
+from speech import generate_speech
+
 class TicketPrices:
     def __init__(self):
         self.model = "gpt-4o-mini"
-        self.tools = [{"type": "function", "function": self.ticket_price_function}]
         self._set_system_message()
         self._set_ticket_prices()
         self._set_ticket_price_function()
+        self.tools = [{"type": "function", "function": self.ticket_price_function}]
         
     def _set_system_message(self):
         self.system_message =   "You are a helpful assistant for an Airline called FlightAI. " \
@@ -40,3 +46,43 @@ class TicketPrices:
                 "additionalProperties": False
             }
         }
+    
+    def get_ticket_price(self, p_destination):
+        return self.ticket_prices.get(p_destination.lower(), "Unknown")
+        
+    def _handle_tool_call(self, message):
+        tool_call = message.tool_calls[0]
+        arguments = json.loads(tool_call.function.arguments)
+        city = arguments.get('destination_city')
+        price = self.get_ticket_price(city)
+        response = {
+            "role": "tool",
+            "content": json.dumps({"destination_city": city,"price": price}),
+            "tool_call_id": tool_call.id
+        }
+        return response, city
+
+    def _generate_image_prompt(self, destination):
+        return f"An image, in an vibrant pop-art style, representing a vacation in {destination}, showing tourist spots and everything unique about {destination}."
+        
+        
+    def generate_response(self, p_history):
+        all_messages = [{"role": "system", "content": self.system_message}] + p_history
+        openai = OpenAI(api_key=retrieve_api_key_value("OPENAI_API_KEY"))
+        response = openai.chat.completions.create(model=self.model, messages=all_messages, tools=self.tools)
+        image = None
+        
+        if response.choices[0].finish_reason=="tool_calls":
+            message = response.choices[0].message
+            response, city = self._handle_tool_call(message)
+            all_messages.append(message)
+            all_messages.append(response)
+            image = generate_image(self._generate_image_prompt(city))
+            response = openai.chat.completions.create(model=self.model, messages=all_messages)
+            
+        reply = response.choices[0].message.content
+        p_history += [{"role":"assistant", "content":reply}]
+        
+        generate_speech(reply)
+        
+        return p_history, image
